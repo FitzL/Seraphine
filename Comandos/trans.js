16c5a689523b975aff901390a417a09aeaffeebe
Fitzgerald Garcia <82980326+FitzL@users.noreply.github.com>
2025-03-27 22:19:45 -0400

    me voy a matar II

---
+++ Comandos/trans.js
@@ -0,0 +1,59 @@
+﻿const { Command } = require("../modulos/MCommand.js");
+
+const {
+    translate,
+    Translator,
+    speak,
+    singleTranslate,
+    batchTranslate,
+    languages,
+    isSupported,
+    getCode
+} = require('google-translate-api-x');
+
+prototype = {
+    alias: ["tr", "translate", "trad", "traducir"], //nombre del comando
+    descripcion: "Traduce un mensaje", // que hace
+    costo: 1, //cuanto cuesta
+    testing: false, //se está probando?
+    callback: async (args, message, client, system) => {
+        let lang = args.pop();
+        translation = "";
+
+
+        try {
+            await translate(args.slice().join(" "), { to: lang }).then((res) => {
+                translation = res.text;
+            })
+        } catch (e) {
+            args.push(lang);
+            await translate(args.join(" "), { to: 'es' }).then((res) => {
+                translation = res.text;
+            })
+        }
+
+        target = message.member;
+        message.delete();
+
+        let webhookUsername = target.displayName || target.username;
+        let pfp = target.user.avatarURL().toString();
+
+        const webhook = await message.channel.createWebhook({
+            name: webhookUsername,
+            avatar: pfp,
+        })
+        await webhook.send(translation);
+        webhook.delete();
+    }
+}
+
+let command = new Command(
+    prototype.alias,
+    prototype.descripcion,
+    prototype.costo,
+    prototype.testing,
+    prototype.callback,
+    prototype.init
+)
+
+module.exports = command;
---
+++ modulos/MCommand.js
@@ -0,0 +1,29 @@
+﻿class Command {
+    alias = [];
+    descripcion = "";
+    costo = 0;
+    testing = false;
+    init = async () => { };
+    callback = async () => { };
+    cantidad = 1;
+
+    constructor(_alias = [], _descripcion = "", _costo = 0, _testing = false, _callback = async () => { }, _init = async () => { }) {
+        this.alias = _alias;
+        this.descripcion = _descripcion;
+        this.costo = _costo;
+        this.testing = _testing;
+        this.callback = _callback;
+    }
+
+    calculateCosto(multiplier = this.cantidad, bypass = false) {
+        if (isNaN(parseInt(multiplier))) throw "MULTIPLIER_IS_NAN";
+        if (parseInt(multiplier) < 0 && !bypass) throw "MULTIPLIER_IS_NEGATIVE";
+        return this.costo * multiplier;
+    }
+
+    checkCosto(user, multiplier = this.cantidad, bypass) {
+        return user.currency > this.calculateCosto(multiplier);
+    }
+}
+
+module.exports.Command = Command;
