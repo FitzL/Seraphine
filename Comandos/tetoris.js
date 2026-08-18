8a5b66fa9487122c3563425a0b0c03e5e6a5a8c0
FitzL <82980326+FitzL@users.noreply.github.com>
2026-08-18 18:25:04 -0400

    fix

--- Comandos/tetoris.js
+++ Comandos/tetoris.js
@@ -27,8 +27,8 @@ prototype = {
         { name: "Top Global League:", value: (userSummary.league.standing == -1 ? "N/A" : userSummary.league.standing).toString(), inline: true },
         { name: "Top Local league:", value: (userSummary.league.standing_local == -1 ? "N/A" : userSummary.league.standing_local).toString(), inline: true },
         { name: '\u200b', value: '\u200b', inline: true },
-        { name: "League TR:", value: (userSummary.league.tr == -1 ? "N/A" : userSummary.league.tr).toString(), inline: true },
-        { name: "League ELO:", value: (userSummary.league.glicko == -1 ? "N/A" : userSummary.league.glicko).toString(), inline: true },
+        { name: "League TR:", value: (userSummary.league.tr == -1 ? "N/A" : Number.ParseFloat(userSummary.league.tr).toFixed(2).toString()).toString(), inline: true },
+        { name: "League ELO:", value: (userSummary.league.glicko == -1 ? "N/A" : Number.ParseFloat(userSummary.league.glicko).toFixed(2)).toString(), inline: true },
         { name: '\u200b', value: '\u200b', inline: true },
       );
     message.channel.send({ embeds: [embed] })
